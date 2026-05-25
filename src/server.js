32a3f4db91e1af9e95f73ca40e816229a8bb2672
truongdev1503 <127965911+truongdev1503@users.noreply.github.com>
2026-05-25 15:23:10 +0700

    create env

--- src/server.js
+++ src/server.js
@@ -1,27 +1,31 @@
+/* eslint-disable no-console */
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
-
-const app = express()
-
-const hostname = 'localhost'
-const port = 8017
-
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
-
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello, I am running at http://${ hostname }:${ port }/`)
-})
+import exitHook from 'async-exit-hook'
+import { env } from '~/config/environment'
+import { CONNECT_DB } from '~/config/mongodb'
+const START_SERVER = () => {
+  const app = express()
+  const hostname = 'localhost'
+  const port = 8017
+  app.get('/', async (req, res) => {
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+  app.listen(port, hostname, () => {
+    console.log(
+      `Hello ${env.AUTHOR}, I am running at http://${hostname}:${port}/`
+    )
+  })
+  exitHook(() => {
+    console.log('Exit App')
+  })
+}
+;(async () => {
+  try {
+    console.log('Connecting to MongGoDb')
+    await CONNECT_DB()
+    console.log('Connected to MongoDb')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+  }
+})()

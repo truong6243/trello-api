f84e494f590c96e640cd3f935fd4e797f7975f8f
truongdev1503 <127965911+truongdev1503@users.noreply.github.com>
2026-06-04 16:30:55 +0700

    api_create

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,11 +1,18 @@
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 const createNew = async (reqBody) => {
   try {
     const newBoard = {
       ...reqBody,
       slug: slugify(reqBody.title)
     }
-    return newBoard
+
+    // gọi tới tầng model để xử lý lưu bản ghi newBoard vào DB
+    const createdBoard = await boardModel.createNew(newBoard)
+    // lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
+    const getNewBoard = boardModel.findOnebyId(createdBoard.insertedId)
+    // trả về kết quả, trong service luôn phải return
+    return getNewBoard
   } catch (error) {
     //
   }

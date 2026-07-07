18f0c68142315bd17e6949dea5d122d30ac68d88
truongdev1503 <127965911+truongdev1503@users.noreply.github.com>
2026-07-07 17:02:00 +0700

    auth_register_api

---
+++ src/models/userModel.js
@@ -0,0 +1,100 @@
+import Joi from 'joi'
+import { GET_DB } from '~/config/mongodb'
+import { ObjectId } from 'mongodb'
+import { EMAIL_RULE, EMAIL_RULE_MESSAGE } from '~/utils/validators'
+
+const USER_ROLES = {
+  CLIENT: 'client',
+  ADMIN: 'admin'
+}
+
+const USER_COLLECTION_NAME = 'users'
+const USER_COLLECTION_CHEMA = Joi.object({
+  email: Joi.string()
+    .required()
+    .pattern(EMAIL_RULE)
+    .message(EMAIL_RULE_MESSAGE),
+  password: Joi.string().required(),
+  username: Joi.string().required().trim().strict(),
+  displayName: Joi.string().required().trim().strict(),
+  avatar: Joi.string().default(null),
+  role: Joi.string()
+    .valid(USER_ROLES.CLIENT, USER_ROLES.ADMIN)
+    .default(USER_ROLES.CLIENT),
+
+  isActive: Joi.boolean().default(false),
+  verifyToken: Joi.string(),
+
+  createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+const INVALID_UPDATE_FIELDS = ['_id', 'email', 'username', 'createAt']
+
+const validateBeforeCreate = async (data) => {
+  return await USER_COLLECTION_CHEMA.validateAsync(data, { abortEarly: false })
+}
+
+const createNew = async (data) => {
+  try {
+    const validData = await validateBeforeCreate(data)
+    const createdUser = await GET_DB()
+      .collection(USER_COLLECTION_NAME)
+      .insertOne(validData)
+    return createdUser
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOnebyId = async (userId) => {
+  try {
+    const result = await GET_DB().collection(USER_COLLECTION_NAME).findOne({
+      _id: new ObjectId(userId)
+    })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOnebyEmail = async (emailValue) => {
+  try {
+    const result = await GET_DB().collection(USER_COLLECTION_NAME).findOne({
+      email: emailValue
+    })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const update = async (userId, updateData) => {
+  Object.keys(updateData).forEach((fieldName) => {
+    if (INVALID_UPDATE_FIELDS.includes(fieldName)) {
+      delete updateData[fieldName]
+    }
+  })
+  try {
+    const updateBoard = await GET_DB()
+      .collection(USER_COLLECTION_NAME)
+      .findOneAndUpdate(
+        { _id: new ObjectId(userId) },
+        { $set: updateData },
+        { returnDocument: 'after' }
+      )
+    return updateBoard
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+export const userModel = {
+  USER_COLLECTION_NAME,
+  USER_COLLECTION_CHEMA,
+  createNew,
+  findOnebyId,
+  findOnebyEmail,
+  update
+}
